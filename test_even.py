3f4f5e212ebd0ad60437b9dfe25dd376d902af84
Akil <akhil.11101999@gmail.com>
2024-11-27 15:09:23 +0530

    adding 2 files

---
+++ test_even.py
@@ -0,0 +1,12 @@
+from check_even import is_even
+
+def test_is_even():
+    assert is_even(18), 'Number is Odd'
+
+"""
+1.identify the location of jenkins.war file
+2.launch : java -jar Jenkins.war 
+"""
+
+# import sys
+# print(sys.version)
