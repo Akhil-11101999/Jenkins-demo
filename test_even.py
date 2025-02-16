4bbdb6c721a3692ef42abdaa82f63ae4836140df
Akil <akhil.11101999@gmail.com>
2025-02-16 20:29:33 +0530

    change 200

--- test_even.py
+++ test_even.py
@@ -4,7 +4,7 @@ def test_is_even():
     assert is_even(100), 'Number is Odd'
 
 def test_is_odd():
-    assert is_odd(100), 'Number is even'
+    assert is_odd(200), 'Number is even'
 
 """
 1.identify the location of jenkins.war file
